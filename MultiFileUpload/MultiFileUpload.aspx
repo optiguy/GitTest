18fd6a1652150dafb0740dad9c26c0b77b5e5d0d
bbr <bbr@rts.dk>
2016-09-30 11:13:35 +0200

    Initial commit

---
+++ MultiFileUpload/MultiFileUpload.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MultiFileUpload.aspx.cs" Inherits="MultiFileUpload.MultiFileUpload" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>Upload more than one file</h1>
+        <asp:FileUpload ID="Fileupload_multi" AllowMultiple="true" runat="server" accepts="image/jpeg|image/png" />
+        <asp:Button Text="Upload" runat="server" OnClick="Button_upload_Click" />
+        <asp:Literal ID="Literal_message" runat="server"></asp:Literal>
+    </form>
+</body>
+</html>
---
+++ MultiFileUpload/MultiFileUpload.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MultiFileUpload
+{
+    public partial class MultiFileUpload : System.Web.UI.Page
+    {
+        private List<string> messages = new List<string>();
+        private int maxAllowedFileSize = 1024 * 1000 * 12;
+        private List<string> allowedFileTypes = new List<string>(){
+            {"image/jpeg"},
+            {"image/gif"},
+            {"audio/mpeg3"},
+            {"video/mp4"}
+        };
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button_upload_Click(object sender, EventArgs e)
+        {
+            
+            foreach(HttpPostedFile file in Fileupload_multi.PostedFiles)
+            {
+                //Tjek på den enkelte fils størrelse
+                if (file.ContentLength >= maxAllowedFileSize)
+                {
+                    messages.Add(String.Format("Filen <strong>{0}</strong> er for stor!", file.FileName));
+                    return;
+                }
+
+                if (!allowedFileTypes.Contains(file.ContentType))
+                {
+                    messages.Add(String.Format("Filtypen {0} er ikke tilladt for fil {1}", file.ContentType, file.FileName));
+                    return;
+                }
+
+                Guid uniktId = Guid.NewGuid();
+                string nytNavn = uniktId.ToString() + "_" + file.FileName;
+            }
+            ShowMessages();
+        }
+
+        private void ShowMessages()
+        {
+            string html = "";
+            foreach (string message in messages)
+            {
+                html += string.Format("<li>{0}</li>", message);
+            }
+            Literal_message.Text = "<ul>" + html + "</ul>";
+        }
+    }
+}
